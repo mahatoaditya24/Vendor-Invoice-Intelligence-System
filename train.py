f8fedd5825f7d8563ecca7af0a824482969ba04e
mahatoaditya24 <adityamahato675@gmail.com>
2026-08-07 00:09:18 +0530

    Add files via upload
    Invoice flagging

--- train.py
+++ train.py
@@ -1,120 +1,55 @@
 import joblib
 from pathlib import Path
-
 from data_preprocessing import (
-    load_vendor_invoice_data,
-    prepare_features,
-    split_data
+    load_invoice_data,
+    apply_label,
+    split_data,
+    scale_features
 )
+from modeling_evaluation import train_random_forest, evaluate_classifier
 
-from model_evaluation import (
-    train_linear_regression,
-    train_decision_tree,
-    train_random_forest,
-    evaluate_model
-)
+FEATURES = [
+    "invoice_quantity",
+    "invoice_dollars",
+    "Freight",
+    "total_item_quantity",
+     "total_item_dollars"
+]
 
+TARGET = "flag_invoice"
 
 def main():
-
-    db_path = r"C:\Users\ADITYA\Downloads\inventory.db"
-
+    db_path =r"C:\Users\ADITYA\Downloads\inventory.db"
     model_dir = Path("models")
     model_dir.mkdir(exist_ok=True)
 
-    # Load dataset
-    df = load_vendor_invoice_data(db_path)
 
-    # Feature engineering
-    X, y = prepare_features(df)
+    #Load data
+    df = load_invoice_data()
+    df = apply_label(df)
 
-    # Train-test split
-    X_train, X_test, y_train, y_test = split_data(X, y)
+    #Prepare data
+    X_train, X_test, y_train, y_test = split_data(df, FEATURES, TARGET)
+    X_train_scaled, X_test_scaled = scale_features(
+        X_train, X_test, 'models/scaler.pkl'
+    ) 
 
+    #Train and evaluate models
+    grid_search = train_random_forest(X_train_scaled, y_train)
 
-    # Train models
-    lr_model = train_linear_regression(
-        X_train, y_train
+    evaluate_classifier(
+        grid_search.best_estimator_,
+        X_test_scaled,
+        y_test,
+        "Random Forest Classifier"
     )
 
-    dt_model = train_decision_tree(
-        X_train, y_train
-    )
-
-    rf_model = train_random_forest(
-        X_train, y_train
-    )
-
-
-    # Evaluate models
-    results = []
-
-    results.append(
-        evaluate_model(
-            lr_model,
-            X_test,
-            y_test,
-            "Linear Regression"
-        )
-    )
-
-    results.append(
-        evaluate_model(
-            dt_model,
-            X_test,
-            y_test,
-            "Decision Tree Regression"
-        )
-    )
-
-    results.append(
-        evaluate_model(
-            rf_model,
-            X_test,
-            y_test,
-            "Random Forest Regression"
-        )
-    )
-
-
-    # Print results
-    print("\nModel Performance:")
-    for result in results:
-        print(result)
-
-
-    # Select best model based on lowest MAE
-    best_model_info = min(
-        results,
-        key=lambda x: x["mae"]
-    )
-
-
-    best_model_name = best_model_info["model_name"]
-
-
-    models = {
-        "Linear Regression": lr_model,
-        "Decision Tree Regression": dt_model,
-        "Random Forest Regression": rf_model
-    }
-
-
-    best_model = models[best_model_name]
-
-
-    # Save model
-    model_path = model_dir / "predict_freight_model.pkl"
-
-    joblib.dump(
-        best_model,
-        model_path
-    )
-
-
-    print("\nBest Model:", best_model_name)
-    print("Saved Path:", model_path)
+    #Save the best model
+    joblib.dump(grid_search.best_estimator_, 'models/predict_flag_invoice.pkl')
 
 
 if __name__ == "__main__":
     main()
+
+
+    
